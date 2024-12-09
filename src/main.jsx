7d79a3108a3ec84d6111871c8e1744e38a4908b5
waseem <waseemsafdar98@gmail.com>
2024-12-09 19:37:44 +0500

    fixes for urls

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Suspense>
-    <BrowserRouter>
+    <BrowserRouter basename="/dispatcher-frontend">
       <App />
     </BrowserRouter>
   </Suspense>,
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import svgr from '@svgr/rollup';
 // https://vitejs.dev/config/
 export default defineConfig({
     build: { chunkSizeWarningLimit: 600, },
-    base: '/',
+    base: '/dispatcher-frontend/',
     resolve: {
         alias: {
             src: resolve(__dirname, 'src'),
