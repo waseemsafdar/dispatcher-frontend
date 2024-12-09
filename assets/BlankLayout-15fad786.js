1c19fa0e5380093387b569c0f87f5bd78f704c6b
waseem <waseemsafdar98@gmail.com>
2024-12-09 19:22:20 +0500

    Updates

--- assets/BlankLayout-15fad786.js
+++
@@ -1,1 +0,0 @@
-import{j as t,W as s}from"./index-4b681f2b.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx(s,{})});export{e as default};
---
+++ assets/BlankLayout-48992b53.js
@@ -0,0 +1,1 @@
+import{j as t,W as s}from"./index-04482d74.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx(s,{})});export{e as default};
