3926933427ca5d460babe2ade3e28090af9f9361
waseem <waseemsafdar98@gmail.com>
2024-12-09 19:21:29 +0500

    changes vite

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import svgr from '@svgr/rollup';
 // https://vitejs.dev/config/
 export default defineConfig({
     build: { chunkSizeWarningLimit: 600, },
+    base: 'https://waseemsafdar.github.io/dispatcher-frontend/',
     resolve: {
         alias: {
             src: resolve(__dirname, 'src'),
