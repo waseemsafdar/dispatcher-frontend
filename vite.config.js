058ea86fff1e1c40e1be43b9bda965e17ae5c313
waseem <waseemsafdar98@gmail.com>
2024-12-09 19:03:03 +0500

    changes for deploy

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: { chunkSizeWarningLimit: 600, },
     resolve: {
         alias: {
             src: resolve(__dirname, 'src'),
