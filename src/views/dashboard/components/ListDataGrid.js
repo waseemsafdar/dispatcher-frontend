92c4775a5ac7b96884f6fe695a1f3f35b6a27d60
waseem <waseemsafdar98@gmail.com>
2025-01-08 22:07:42 +0500

    fixes

--- src/views/dashboard/components/ListDataGrid.js
+++ src/views/dashboard/components/ListDataGrid.js
@@ -1,16 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { DataGrid } from '@mui/x-data-grid';
 import { deleteLoadById, getLoad } from '../../../store/loadSlice';
-import { IconButton } from '@mui/material';
+import { 
+  IconButton, 
+  Table, 
+  TableBody, 
+  TableCell, 
+  TableContainer, 
+  TableHead, 
+  TableRow,
+  Paper,
+  TablePagination,
+  TableSortLabel,
+  Box
+} from '@mui/material';
 import { IconEdit, IconEye, IconTrash } from '@tabler/icons-react';
 import { useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
-import { height } from '@mui/system';
+import { textAlign } from '@mui/system';
 
 const ListDataGrid = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('cpm');
 
   const { loadList, status, error } = useSelector((state) => state.load);
 
@@ -22,7 +37,7 @@ const ListDataGrid = () => {
     dispatch(deleteLoadById(id)).unwrap()
       .then(() => {
         toast('Load deleted successfully!');
-        dispatch(getLoad()); // Refresh the list after deletion
+        dispatch(getLoad());
       })
       .catch((err) => {
         toast('Failed to delete load:', err);
@@ -33,62 +48,48 @@ const ListDataGrid = () => {
     navigate(`/load-detail/${id}`);
   };
 
-  const columns = [
-    { field: 'cpm', headerName: 'CPM', width: 90 },
-    { field: 'customer_load', headerName: 'Customer Load', width: 180 },
-    { field: 'freight_amount', headerName: 'Freight Amount', width: 150 },
-    { field: 'expected_dispatcher', headerName: 'Planned Dispatcher', width: 180 },
-    { field: 'expected_vehicle', headerName: 'Planned Vehicle', width: 180 },
-    { field: 'partner_id', headerName: 'Partner', width: 130 },
-    { field: 'trailer_type', headerName: 'Trailer Type', width: 150 },
-    { field: 'pickup_city', headerName: 'Pickup City', width: 150 },
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-    { field: 'pickup_state', headerName: 'Pickup State', width: 150 },
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
-    { field: 'delivery_city', headerName: 'Delivery City', width: 150 },
+  const handleRequestSort = (property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
 
-    { field: 'delivery_state', headerName: 'Delivery State', width: 150 },
-    { field: 'planned_start_time', headerName: 'Planned Start Time', width: 150 },
+  const stableSort = (array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  };
 
-    { field: 'planned_end_time', headerName: 'Planned End Time', width: 150 },
-    { field: 'is_archived', headerName: 'Status', width: 150 },
+  const getComparator = (order, orderBy) => {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  };
 
-
-    {
-      field: 'actions',
-      headerName: 'Actions',
-      width: 150,
-      renderCell: (params) => (
-        <>
-          <IconButton
-            onClick={() => handleEdit(params.id)}
-            aria-label="edit"
-          >
-            <IconEdit />
-          </IconButton>
-          {/* <IconButton
-            onClick={() => handleDelete(params.id)}
-            aria-label="delete"
-          >
-            <IconTrash />
-          </IconButton> */}
-          <IconButton
-            onClick={() => handleRowClick(params.id)}
-            aria-label="detail"
-          >
-            <IconEye />
-          </IconButton>
-        </>
-      ),
-    },
-  ];
-
+  const descendingComparator = (a, b, orderBy) => {
+    if (b[orderBy] < a[orderBy]) return -1;
+    if (b[orderBy] > a[orderBy]) return 1;
+    return 0;
+  };
 
   useEffect(() => {
-    if (status === 'idle') {
+    //if (status === 'idle') {
       dispatch(getLoad());
-    }
-  }, [dispatch, status]);
+    //}
+  }, [dispatch]);
 
   if (status === 'loading') return <div>Loading...</div>;
   if (status === 'failed') return <div>Error: {error}</div>;
@@ -102,29 +103,112 @@ const ListDataGrid = () => {
     expected_vehicle: load.expected_vehicle,
     partner_id: load?.partner?.name,
     pickup_city: load?.pickup_city,
-    pickup_state:  load?.pickup_state,
-    delivery_city:  load?.delivery_city,
-    delivery_state:  load?.delivery_state,
-    planned_start_time:  load?.planned_start_time,
-    planned_end_time:  load?.planned_end_time,
+    pickup_state: load?.pickup_state,
+    delivery_city: load?.delivery_city,
+    delivery_state: load?.delivery_state,
+    planned_start_time: load?.planned_start_time,
+    planned_end_time: load?.planned_end_time,
     is_archived: load?.is_archived ? 'InActive' : 'Active',
     trailer_type: load.trailer_type.map(type => type.type).join(', '),
-  }));
+  })) || []; // Ensure rows is an empty array if loadList is undefined
+
+  const columns = [
+    { id: 'actions', label: 'Actions', minWidth: 100 },
+    { id: 'cpm', label: 'CPM', minWidth: 90 },
+    { id: 'customer_load', label: 'Customer Load', minWidth: 120 },
+    { id: 'freight_amount', label: 'Freight Amount', minWidth: 120 },
+    { id: 'expected_dispatcher', label: 'Planned Dispatcher', minWidth: 120 },
+    { id: 'expected_vehicle', label: 'Planned Vehicle', minWidth: 120 },
+    { id: 'partner_id', label: 'Partner', minWidth: 100 },
+    { id: 'trailer_type', label: 'Trailer Type', minWidth: 120 },
+    { id: 'pickup_city', label: 'Pickup City', minWidth: 120 },
+    { id: 'pickup_state', label: 'Pickup State', minWidth: 120 },
+    { id: 'delivery_city', label: 'Delivery City', minWidth: 120 },
+    { id: 'delivery_state', label: 'Delivery State', minWidth: 120 },
+    { id: 'planned_start_time', label: 'Planned Start Time', minWidth: 120 },
+    { id: 'planned_end_time', label: 'Planned End Time', minWidth: 120 },
+    { id: 'is_archived', label: 'Status', minWidth: 100 }
+  ];
 
   return (
-    <div style={{ height: "400px", width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={10}
+    <Box>
+      <TableContainer component={Paper}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  sx={{
+                    minWidth: column.minWidth,
+                    border: '1px solid rgba(48, 51, 61, 0.24)',
+                    backgroundColor: '#303f69f0',
+                    color: 'white !important',
+                    fontWeight: 'bold',
+                    padding: '5px',
+                    lineHeight:'20px'
+                  }}
+                  sortDirection={orderBy === column.id ? order : false}
+                >
+                  <TableSortLabel
+                    active={orderBy === column.id}
+                    direction={orderBy === column.id ? order : 'asc'}
+                    onClick={() => handleRequestSort(column.id)}
+                    sx={{ color: 'white', '&.MuiTableSortLabel-root.MuiTableSortLabel-active': { color: 'white' } }}
+                  >
+                    {column.label}
+                    {orderBy === column.id ? (
+                      <Box component="span" sx={{ visuallyHidden: true }}>
+                        {order === 'desc' ? '' : ''}
+                      </Box>
+                    ) : null}
+                  </TableSortLabel>
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {stableSort(rows, getComparator(order, orderBy))
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} sx={{ border: '1px solid #d3d3d3', padding: '5px' }}>
+                        {column.id === 'actions' ? (
+                          <>
+                            <IconButton onClick={() => handleEdit(row.id)} aria-label="edit">
+                              <IconEdit />
+                            </IconButton>
+                            {/* <IconButton onClick={() => handleDelete(row.id)} aria-label="delete">
+                              <IconTrash />
+                            </IconButton> */}
+                            <IconButton onClick={() => handleRowClick(row.id)} aria-label="detail">
+                              <IconEye />
+                            </IconButton>
+                          </>
+                        ) : (
+                          value
+                        )}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
         rowsPerPageOptions={[5, 10, 20]}
-        pagination
-        disableSelectionOnClick
-        scrollbarSize={10}
-        style={{ overflowX: 'auto' }}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
       />
-
-    </div>
+    </Box>
   );
 };
 
