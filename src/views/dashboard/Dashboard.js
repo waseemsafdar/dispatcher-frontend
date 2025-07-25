1e4a9a2800b65ddfdeb83847ffc50b2a5640b72d
waseem <waseemsafdar98@gmail.com>
2025-07-25 21:01:24 +0500

    new fixes

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -14,6 +14,7 @@ const Dashboard = () => {
   const handleClearFilters = () => { 
     dispatch(clearFilters());
     dispatch(getLoad({ filters:{}}));
+    
   };
   const handleFilterSubmit = (filters) => {
     const formattedData = {
